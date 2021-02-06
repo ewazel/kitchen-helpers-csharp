9654bf0c6cfe9037270248303f2f45109c7d1240
Ewa Zelenska <ewa.zelenska@gmail.com>
2021-02-06 09:25:26 +0100

    first commit

---
+++ src/Codecool.KitchenManagement/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace KitchenManagement
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Ramsay: WHAT ARE YOU?");
+            Console.WriteLine("Kitchen helper: An idiot sandwich");
+            Console.WriteLine("Ramsay: IDIOT SANDWICH WHAT?");
+            Console.WriteLine("Kitchen helper: An idiot sandwich, chef Ramsay");
+        }
+    }
+}
