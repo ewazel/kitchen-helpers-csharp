33f3c56f26aff86751730414a49cd594fad7ea7f
Ewa Zelenska <ewa.zelenska@gmail.com>
2021-02-06 13:25:56 +0100

    First UnitTest done & passed

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,35 @@
+using System;
+using Codecool.KitchenManagement.Equipment;
+using KitchenManagement.Employees;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TryToGiveIngredient_WhenHelperHasIngredient_ReturnsTrue()
+        {
+            bool validHelper = false;
+            Ingredient ingredient = Ingredient.Carrot;
+            bool result = false;
+
+            while (!validHelper)
+            {
+                KitchenHelper helper = new KitchenHelper(null, DateTime.Today, 0, null);
+                if (helper.hasIngredient(ingredient))
+                {
+                    result = helper.TryToGiveIngredient(ingredient);
+                    validHelper = true;
+                }
+            }
+            
+            Assert.AreEqual(true, result);
+        }
+    }
+}
